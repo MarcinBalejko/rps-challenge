1a9c7ca1cde03b72a1af476e36c2c0c3b8b979f9
MarcinBalejko <mbbm9@o2.pl>
2019-07-21 20:41:17 +0200

    :construction: Play mode

--- app.rb
+++ app.rb
@@ -1,9 +1,22 @@
 require 'sinatra/base'
+require './lib/game'
+require './lib/player'
 
 class Rps < Sinatra::Base
+    enable :sessions
 
     get '/' do
         erb :index
+    end
+
+    post '/names' do
+        player_1 = Player.new(params[:player_1_name])
+        $game = Game.new(player_1)
+        redirect '/play'
+    end
+
+    get '/play' do
+        erb :play
     end
 
     run! if app_file == $0
