5c518ba0c5fc9f31ad5452841810a9a006681eaa
MarcinBalejko <mbbm9@o2.pl>
2019-07-21 20:38:13 +0200

    :white_check_mark: Player can enter his/her name

---
+++ spec/features/player_name_spec.rb
@@ -0,0 +1,8 @@
+feature 'Player can enter his/her name' do
+    scenario 'enters the name and switches to the next page' do
+        visit('/')
+        fill_in :player_1_name, with: 'Marcin'
+        click_button 'Start'
+        expect(page).to have_content 'Start!'
+    end
+end
