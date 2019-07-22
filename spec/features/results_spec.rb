304bab118b3a63c2d775cb9ba4d4f60811fe2703
MarcinBalejko <mbbm9@o2.pl>
2019-07-22 21:21:46 +0200

    :white_check_mark: Can enter the results page

--- spec/features/results_spec.rb
+++ spec/features/results_spec.rb
@@ -3,7 +3,7 @@ feature 'Open results page' do
         visit('/')
         fill_in :player_1_name, with: 'Marcin'
         click_button 'Start'
-        click_link('rock')
+        click_button('rock')
         expect(page).to have_content 'Results'
     end
 end
