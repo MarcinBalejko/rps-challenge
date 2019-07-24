aea5e40a549993ac7ae9ab237d78cccb74e3001a
MarcinBalejko <mbbm9@o2.pl>
2019-07-24 20:28:58 +0200

    :white_check_mark: Can select one of three options

--- spec/features/results_spec.rb
+++ spec/features/results_spec.rb
@@ -1,9 +1,14 @@
 feature 'Open results page' do
     scenario 'can enter the results page' do
-        visit('/')
-        fill_in :player_1_name, with: 'Marcin'
-        click_button 'Start'
+        sign_in_and_play
         click_button('rock')
         expect(page).to have_content 'Results'
+    end
+end
+feature 'Saves selected option' do
+    scenario 'Can select one of the 3 options' do
+        sign_in_and_play
+        click_button('paper')
+        expect(page).to have_css("img[src*='images/paper.jpg']")
     end
 end
