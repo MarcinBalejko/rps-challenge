cba8edf982b726d36cf4c73fe7aa73db1a2caf98
MarcinBalejko <mbbm9@o2.pl>
2019-07-25 02:29:14 +0200

    :recycle: refactored

--- spec/features/results_spec.rb
+++ spec/features/results_spec.rb
@@ -12,3 +12,11 @@ feature 'Saves selected option' do
         expect(page).to have_css("img[src*='images/paper.jpg']")
     end
 end
+feature 'Player can win' do
+    scenario 'Player\'s choice beats computer\'s ' do
+        sign_in_and_play
+        click_button('paper')
+        allow_any_instance_of(Game).to receive(:selector).and_return(:rock)
+        expect(page).to have_content 'You won, Marcin!'
+    end
+end
