783fb5817b3e1a3186cf3572751b7214804bb4ab
MarcinBalejko <mbbm9@o2.pl>
2019-07-28 01:13:30 +0200

    :white_check_mark: Player gets 'Player' name if no name entered

--- spec/features/results_spec.rb
+++ spec/features/results_spec.rb
@@ -9,7 +9,7 @@ feature 'Saves selected option' do
     scenario 'Can select one of the 3 options' do
         sign_in_and_play
         click_button('paper')
-        expect(page).to have_css("img[src*='images/paper.jpg']")
+        expect(page).to have_css("img[src*='hand-paper-solid.svg']")
     end
 end
 feature 'Player can win' do
@@ -18,5 +18,12 @@ feature 'Player can win' do
         click_button('paper')
         allow_any_instance_of(Game).to receive(:selector).and_return(:rock)
         expect(page).to have_content 'You won, Marcin!'
+    end
+end
+feature 'Player gets random name' do
+    scenario 'Player gets \'Player\' name if no name entered' do
+        visit('/')
+        click_button 'Start'
+        expect(page).to have_content 'Let\'s begin, Player!'
     end
 end
